6022679904fa95ddcfed90f9a2f519311561bfbf
Andres Herrero <andresherrerostudio@gmail.com>
2016-06-08 00:19:10 +0300

    add ssl string

--- hr.sh
+++ hr.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn -B -s settings.xml -DskipTests=true clean package
-java -Dspring.profiles.active="jpa,heroku" -DDATABASE_URL="postgresql://postgres:postgres@localhost:5432/postgres" -jar target/dependency/webapp-runner.jar target/*.war
+# mvn -B -s settings.xml -DskipTests=true clean package
+# java -Dspring.profiles.active="jpa,heroku" -DDATABASE_URL="postgresql://postgres:postgres@localhost:5432/postgres" -jar target/dependency/webapp-runner.jar target/*.war
